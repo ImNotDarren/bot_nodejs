3bf99d518af4bd53f0bf48f7fc26bbf4f3acf537
ImNotDarren <sliu178@binghamton.edu>
2023-01-20 13:47:30 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,46 +1,14 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 const amqp = require("amqplib");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var channel, connection;  //global variables
 
 var app = express();
-const router=express.Router()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
+app.get('/', (req, res) => {
+  res.send("main page")
+})
 
 async function connectQueue() {
   try {
@@ -65,7 +33,7 @@ async function sendData(data) {
 
 connectQueue()
 
-router.get("/send-msg", (req, res) => {
+app.get("/send-msg", (req, res) => {
 
   // data to be sent
   const data = {
@@ -78,6 +46,6 @@ router.get("/send-msg", (req, res) => {
 
 })
 
+app.listen(8081)
 
-
-module.exports = app;
+module.exports = app
