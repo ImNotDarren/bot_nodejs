624919db1f3b1f0140018861a1c568bad4dd2e78
ImNotDarren <sliu178@binghamton.edu>
2023-01-20 13:32:29 -0500

    update

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const amqp = require("amqplib");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var channel, connection;  //global variables
 
 var app = express();
+const router=express.Router()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,12 +26,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -37,5 +40,44 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+async function connectQueue() {
+  try {
+    connection = await amqp.connect("amqp://localhost:5672");
+    channel = await connection.createChannel()
+
+    await channel.assertQueue("test-queue")
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+async function sendData(data) {
+  // send data to queue
+  await channel.sendToQueue("test-queue", Buffer.from(JSON.stringify(data)));
+
+  // close the channel and connection
+  await channel.close();
+  await connection.close();
+}
+
+connectQueue()
+
+router.get("/send-msg", (req, res) => {
+
+  // data to be sent
+  const data = {
+    title: "Six of Crows",
+    author: "Leigh Burdugo"
+  }
+  sendData(data);  // pass the data to the function we defined
+  console.log("A message is sent to queue")
+  res.send("Message Sent"); //response to the API request
+
+})
+
+
 
 module.exports = app;
